39515378e5811f89beb9bc41c5b8692c80393fe4
Kevin Weng <kweng@mit.edu>
2018-11-14 19:03:50 -0500

    added initial working experiment procedure

--- app/src/components/Experiment.js
+++ app/src/components/Experiment.js
@@ -4,23 +4,58 @@ class Experiment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageUrls: []
+      images: [],
+      imageIdx: 0,
+      hideImage: false
     };
+
+    this.showButtons = this.showButtons.bind(this)
+    this.nextImage = this.nextImage.bind(this)
   }
 
   componentDidMount() {
     fetch("http://localhost:5000/images")
     .then(r => r.json())
-    .then(r => { this.setState({ imageUrls: r }) })
+    .then(r => { this.setState({ images: r }) })
+
+    this.timeout = setTimeout(
+      this.showButtons,
+      500
+    )
+  }
+
+  showButtons() {
+    this.setState((state) => {
+      return {
+        hideImage: true,
+        imageIdx: state.imageIdx + 1}
+    });
+  }
+
+  nextImage() {
+    if (this.state.imageIdx < this.state.images.length) {
+      this.setState({
+        hideImage: false
+      }, () => {
+        this.timeout = setTimeout(
+          this.showButtons,
+          500
+        )
+      })
+    } else {
+      alert("No more images")
+    }
   }
 
   render() {
-    return (
-      <div>
-        <h1>Welcome to the Experiment!</h1>
-        <img src="http://localhost:5000/images/test.png" alt="test"/>
+    var view = !this.state.hideImage 
+      ? <img src={`http://localhost:5000/images/${this.state.images[this.state.imageIdx]}`} alt="face"/>
+      : <div>
+        <button type='button' onClick={() => { this.nextImage() }}> Real </button>
+        <button type='button' onClick={() => { this.nextImage() }}> Fake </button>
       </div>
-    )
+
+    return <div>{ view }</div>
   }
 }
 
